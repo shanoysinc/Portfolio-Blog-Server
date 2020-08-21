876a9373b2a931bc1cb30e29ce94eb60eefa7a5a
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-08-21 15:26:00 -0500

    add npm run start in package

--- routes/UserRoute/UserRoute.js
+++ routes/UserRoute/UserRoute.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../../controllers/userController/userController");
 
+router.get("/", (req, res) => {
+	res.send("welcome home");
+});
+
 router.post("/signin", userController.signin);
 
 router.post("/signup", userController.signup);
