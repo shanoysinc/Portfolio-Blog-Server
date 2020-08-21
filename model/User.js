ddd1022bee1c36c497f19df2842cd1dcca67c6fe
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-08-21 17:21:55 -0500

    remove password when send ing back user object

--- model/User.js
+++ model/User.js
@@ -13,6 +13,13 @@ const userSchema = new mongoose.Schema({
 	password: { type: String, require: true, minlength: 6 },
 });
 
+userSchema.methods.toJSON = function () {
+	const user = this;
+	const userObject = user.toObject();
+	delete userObject.password;
+	return userObject;
+};
+
 userSchema.pre("save", async function (next) {
 	const user = this;
 
